b986919bcba34cffe08641b765e7e40861aaa095
Joshua Yee <joshyee16@gmail.com>
2024-01-09 10:26:15 -0800

    update db with models and instances, updated associations in model methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Post.create!(title: "Will Pac 12 end on top?", body: "It would be quite a fitting end for the Washington Huskies to win a natty and bring home the championship to the PAC only for the PAC to disband next year.")
+
+# Post.create!(title: "Penix True Heisman?!", body: "We're getting to see now in the CFP semis that Penix might have been the best QB in college football. Daniels had the stats but beat up on bad teams, the old SEC bias might be in play here again.")
+
+# Post.create!(title: "Go Blue!", body: "Michigan is gonna hammer UW today! Their O and D lines are too big and will push around the smaller PAC 12 lines. UM by a million!")
+
+# Post.create!(title: "Hammer the Over?", body: "I feel like the over at 56.5 is a lock. UM is putting up points over UW's paltry defense and it's hard to believe UM will stop Penix from putting up 28 points min.")
+
+# Tag.create!(hashtag: "#PAC12")
+# Tag.create!(hashtag: "#SEC")
+# Tag.create!(hashtag: "#CFPchampionship")
+# Tag.create!(hashtag: "#BIG10")
+# Tag.create!(hashtag: "#betting")
+# Tag.create!(hashtag: "#Heisman")
+# Tag.create!(hashtag: "#CFPsemis")
+
+# PostTag.create!(post_id: 1,tag_id: 2)
+# PostTag.create!(post_id: 1,tag_id: 7)
+# PostTag.create!(post_id: 2,tag_id: 1)
+# PostTag.create!(post_id: 2,tag_id: 3)
+# PostTag.create!(post_id: 3,tag_id: 1)
+# PostTag.create!(post_id: 3,tag_id: 6)
+# PostTag.create!(post_id: 4,tag_id: 3)
+# PostTag.create!(post_id: 4,tag_id: 4)
+# PostTag.create!(post_id: 4,tag_id: 5)
+# PostTag.create!(post_id: 5,tag_id: 1)
+# PostTag.create!(post_id: 5,tag_id: 3)
+# PostTag.create!(post_id: 5,tag_id: 4)
+# PostTag.create!(post_id: 5,tag_id: 5)
+
+
+User.create!(name: "Josh", email: "josh@email.com", password_digest: "password")
+User.create!(name: "Kyle", email: "kyle@email.com", password_digest: "password")
+User.create!(name: "Nate", email: "nate@email.com", password_digest: "password")
+User.create!(name: "Nico", email: "nico@email.com", password_digest: "password")
+User.create!(name: "Kano", email: "kano@email.com", password_digest: "password")
